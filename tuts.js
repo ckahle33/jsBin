bf4c2416096928d0dac93b6eb5b3ba2148ff3dc7
Craig Kahle <craig.kahle@austin.utexas.edu>
2013-10-25 12:02:38 -0500

    appendTo Tuts

---
+++ tuts.js
@@ -0,0 +1,10 @@
+var alice = {size: "small", 
+			"arch-enemy" : "the red queen", 
+			"age" : 7.5 }; 
+
+
+
+console.log( alice.size );
+alice.destiny = "fight the jabberwocky";
+alice.age = 8;
+console.log (alice.desitiny);
